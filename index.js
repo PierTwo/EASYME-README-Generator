a012e7e44fc21f95b38dd4cf34ffa6153364ee22
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-08-13 11:55:48 -0500

    Exported questions and added inquirer with prompts

--- index.js
+++ index.js
@@ -1,7 +1,82 @@
 // TODO: Include packages needed for this application
+const questionsModule = require('./utils/questions');
+const inquirer = require('inquirer');
 
+const questions = questionsModule.questions;
 // TODO: Create a function to initialize app
-function init() { }
+async function init() {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: questions[0],
+            name: 'continue',
+        },
+    ])
+        .then((response) => {
+            if (!response.continue) {
+                console.log('Goodbye!');
+                process.exit()
+            } else {
+                mainPrompts();
+            };
+        });
+
+    const mainPrompts = () => inquirer.prompt([
+        {
+            type: 'confirm',
+            message: questions[1],
+            name: 'tableOfContent',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'descrBuild',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'descrSolve',
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'descrLearn',
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[9],
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: questions[10],
+            name: 'contribution',
+        },
+    ])
+        .then((response) => {
+            console.log(response)
+        });
+};
 
 // Function call to initialize app
 init();
--- utils/questions.js
+++ utils/questions.js
@@ -1,11 +1,11 @@
 const questions = [
-    'Welcome to EASYME! Please leave any sections blank that you do not wish to include.\n------------------------------\nDo you want to continue? [y/n]',
+    'Welcome to EASYME! Please leave any sections blank that you do not wish to include.\n------------------------------\nDo you want to continue?',
+    'Do you want to include a table of contents?',
     'What is the title of your project?',
     'Write a short description of your project:',
     'Why did you build this project?',
     'What problem(s) does it solve?',
     'What did you learn during this project?',
-    'Do you want to include a table of contents? [y/n]',
     'How do you install this project?',
     'How do you use this Project?',
     'How can you test this project?',
@@ -16,3 +16,7 @@ const questions = [
     'What, if any, license do you want to include?'
 
 ];
+
+module.exports = {
+    questions
+};
