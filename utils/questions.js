4ae772f50278e90353fcee77e2da798af9726eb3
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-08-12 22:14:23 -0500

    Created an array of questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,1 +1,18 @@
-const questions = [];
+const questions = [
+    'Welcome to EASYME! Please leave any sections blank that you do not wish to include.\n------------------------------\nDo you want to continue? [y/n]',
+    'What is the title of your project?',
+    'Write a short description of your project:',
+    'Why did you build this project?',
+    'What problem(s) does it solve?',
+    'What did you learn during this project?',
+    'Do you want to include a table of contents? [y/n]',
+    'How do you install this project?',
+    'How do you use this Project?',
+    'How can you test this project?',
+    'How can others contribute to this project?',
+    'Enter a collaborator name followed by a URL then continue, if left blank app will go to the next section.',
+    'Enter a third-party asset name followed by a URL then continue, if left blank app will go to the next section.',
+    'Please enter your GitHub username followed by your email:',
+    'What, if any, license do you want to include?'
+
+];
